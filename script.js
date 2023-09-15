a1434488ba7cc0171af12ed95355d7c0d355f72a
Kama <e.33.dfg@gmail.com>
2023-09-15 15:48:11 +0500

    Pon

---
+++ script.js
@@ -0,0 +1,37 @@
+ var name = prompt("What's your name?")
+ console.log("Your name's " + name)
+ var example = +prompt("4+5 = ?")
+ if(example==9){
+    console.log("Your answer for 1st example is: " +example + " and it is right answer")
+ }
+ else{
+    console.log("Your answer for 1st example is: " +example + " , right answer is: 9")
+ }
+ var examle = +prompt("19-8 = ?")
+ if(examle==11){
+    console.log("Your answer for 2nd example is: " +examle+ " and it is right answer")
+ }
+ else{
+    console.log("Your answer for 2nd example is: " +examle + " , right answer is: 11") 
+ }
+ var exale = +prompt("45*2 = ?")
+ if(exale==90){
+    console.log("Your answer for 3rd example is: " +exale+ " and it is right answer")
+ }
+ else{
+    console.log("Your answer for 3rd example is: " +exale + " , right answer is: 90") 
+ }
+ var exle = +prompt("21/3 = ?")
+ if(exle==7){
+    console.log("Your answer for 4th example is: " +exle+ " and it is right answer")
+ }
+ else{
+    console.log("Your answer for 4th example is: " +exle + " , right answer is: 7") 
+ }
+ var xale = +prompt("14%5 = ?")
+ if(xale==4){
+    console.log("Your answer for 5th example is: " +xale+ " and it is right answer")
+ }
+ else{
+    console.log("Your answer for 5th example is: " +xale + " , right answer is: 4") 
+ }
